1cf65e3470eb7298f4e7c8968526180ca0a8f085
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-31 13:56:53 +0700

    Add files via upload

---
+++ src/NurAzliYT/invmenu/session/PlayerManager.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace NurAzliYT\invmenu\session;
+
+use NurAzliYT\invmenu\InvMenuEventHandler;
+use NurAzliYT\invmenu\session\network\handler\PlayerNetworkHandlerRegistry;
+use pocketmine\Player;
+
+final class PlayerManager{
+
+	/** @var PlayerSession[] */
+	private static $sessions = [];
+
+	public static function create(Player $player) : void{
+		self::$sessions[$player->getRawUniqueId()] = new PlayerSession(
+			$player,
+			new PlayerNetwork(
+				$player,
+				PlayerNetworkHandlerRegistry::get(InvMenuEventHandler::pullCachedDeviceOS($player))
+			)
+		);
+	}
+
+	public static function destroy(Player $player) : void{
+		if(isset(self::$sessions[$uuid = $player->getRawUniqueId()])){
+			self::$sessions[$uuid]->finalize();
+			unset(self::$sessions[$uuid]);
+		}
+	}
+
+	public static function get(Player $player) : ?PlayerSession{
+		return self::$sessions[$player->getRawUniqueId()] ?? null;
+	}
+
+	public static function getNonNullable(Player $player) : PlayerSession{
+		return self::$sessions[$player->getRawUniqueId()];
+	}
+}
